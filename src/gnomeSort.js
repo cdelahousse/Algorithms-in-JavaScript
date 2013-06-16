6212a024a78ef7bcc22b59d92f98523763c2f040
Christian Delahousse <christian@delahousse.ca>
2013-06-15 23:39:32 -0400

    Fixed gnome sort

--- src/gnomeSort.js
+++ src/gnomeSort.js
@@ -1,36 +1,30 @@
-﻿//if JS running in Rhino
-if (typeof Packages === "object" && String(Packages) === "[JavaPackage ]") {
-	load('init.js'); 
+﻿var swap = require('./lib.js').swap;
 
-}
+/**
+ * In place Gnome sort as defined here http://dickgrune.com/Programs/gnomesort.html
+ *
+ * 1. if pair at indices i - 1 and i are in order, examine next pair (increment i index) 
+ * 2. if pair not in order, swap and examine previous pair (decrement index) 
+ *
+ * Boundry cases:
+ *  1. If at left most element, increment index.  
+ *  2. If at right most index, stop.
+ *
+ * @param {array} collection unsorted
+ */
+function gnomeSort (collection) {
+  var len = collection.length,
+      i=0;
 
-sort = extend(sort, {
+  while (i < len) {
+    if (i === 0 || collection[i-1] <= collection[i]){
+      i++;
+    }
+    else {
+      swap(collection,i, i - 1);
+      i--;
+    }
+  }
+} 
 
-	gnomeSort : function (collection) {
-		//Selection sort as defined on http://dickgrune.com/Programs/gnomesort.html:
-		//1. if pair at indices i - 1 and i are in order,
-		//examine next pair (increment i index)
-		//2. if pair not in order, swap and examine previous pair (decrement index)
-		//Boundry cases: 
-		//1. If at left most element, increment index.
-		//2. If at right most index, stop.
-	
-		var len = collection.length,
-				i=0;
-
-		while (i < len) {
-			if (i == 0 || collection[i-1] <= collection[i]){
-				i++;
-			}
-			else {
-				swap(collection,i, i - 1);
-				i--;
-			}
-		}
-
-		return collection;
-	} //end sort
-});
-//Test
-print(sort.gnomeSort(init_collection));
-//TODO Add optmisation found on wikipedia
+if (module && module.exports) module.exports = gnomeSort;
---
+++ tests/gnomeSort.js
@@ -0,0 +1,11 @@
+
+var gnomeSort = require('../src/gnomeSort.js');
+
+var lib = require('./lib.js');
+var assert = lib.assert; 
+var unsorted_collection = lib.unsorted_collection;
+var sortedCollection = lib.sortedCollection; 
+
+var a = unsorted_collection();
+gnomeSort(a);
+assert(a, sortedCollection());
