5695a07f8dc8cef22925458957afa4696966a42e
Christian Delahousse <christian@delahousse.ca>
2013-06-16 00:07:31 -0400

    Removed init.js dep on insersion sort

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,44 +1,39 @@
-﻿var sort = require('./init.js').sort,
-		swap = require('./lib.js').swap,
-		extend = require('./lib.js').extend;
+﻿var swap = require('./lib.js').swap;
 
-sort = extend(sort, {
+function insertionSort (collection) {
+  var len = collection.length,
+      key;
 
-	insertionSort : function (collection) {
-		var len = collection.length,
-		key;
+  for (var j = 0,i ; j < len; j++) {
+    key = collection[j];
 
-		for (var j = 0,i ; j < len; j++) {
-			key = collection[j];
+    i = j - 1; //Sorted parts starts right before current index
+    while (i >= 0 && collection[i] > key){ //Sorted partial result
+      collection[i+1] = collection[i]; //Shift right to make room
+      if (key <= collection[i]) {
+        collection[i] = key;
+      }
+      i--; //Sorting part works backwards
+    }
+  }
+  return collection;
+}
 
-			i = j - 1; //Sorted parts starts right before current index
-			while (i >= 0 && collection[i] > key){ //Sorted partial result
-				collection[i+1] = collection[i]; //Shift right to make room
-				if (key <= collection[i]) {
-					collection[i] = key;
-				}
-				i--; //Sorting part works backwards
-			}
-		}
-		return collection;
-	},
+function insertionSort2 (collection) {
+  var len = collection.length,
+      i,j;
 
-	insertionSort2 : function (collection) {
-		var len = collection.length,
-				i,j;
+  for (i = 1; i < len; i++) {
+    j = i;
+    while (j > 0 && collection[j] < collection[j-1]) {
+      swap(collection,j,j-1);
+      j--;
+    }
+  }
+  return collection;
+}
 
-		for (i = 1; i < len; i++) {
-			j = i;
-			while (j > 0 && collection[j] < collection[j-1]) {
-				swap(collection,j,j-1);
-				j--;
-			}
-		}
-		return collection;
-	}
-
-});
-
-//Test
-var unsorted_collection = require('./lib').unsorted_collection();
-console.log(sort.insertionSort2(unsorted_collection));
+if (module && module.exports) { 
+  module.exports.insertionSort = insertionSort; 
+  module.exports.insertionSort2 = insertionSort2;
+}
