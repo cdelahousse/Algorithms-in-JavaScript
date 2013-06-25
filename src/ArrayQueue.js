b47dbc86e8c5f9fda135feed538bb644207e61c9
Christian Delahousse <christian@delahousse.ca>
2013-06-25 15:08:34 -0400

    fixed whitespace

--- src/ArrayQueue.js
+++ src/ArrayQueue.js
@@ -4,71 +4,71 @@
  * http://opendatastructures.org/ods-java/2_3_ArrayQueue_Array_Based_.html
  */
 function ArrayQueue() {
-	var a = [],
-			n = 0,
-			j = 0;
+  var a = [],
+      n = 0,
+      j = 0;
 
-	function resize() {
-		var b = new Array( Math.max(1, n*2) ),
-				k;
+  function resize() {
+    var b = new Array( Math.max(1, n*2) ),
+        k;
 
-		for (k = 0; k < n; k++) {
-			b[k] = a[ (j + k) % a.length ];
-		}
+    for (k = 0; k < n; k++) {
+      b[k] = a[ (j + k) % a.length ];
+    }
 
-		a = b;
-		j = 0;
-	}
+    a = b;
+    j = 0;
+  }
 
-	this.isEmpty = function() {
-		return n === 0;
-	};
+  this.isEmpty = function() {
+    return n === 0;
+  };
 
-	this.size = function() {
-		return n;
-	};
+  this.size = function() {
+    return n;
+  };
 
-	this.offer = function(x) {
-		return this.add(x);
-	};
+  this.offer = function(x) {
+    return this.add(x);
+  };
 
-	this.add = function(x) {
-		if (n + 1 > a.length) resize();
+  this.add = function(x) {
+    if (n + 1 > a.length) resize();
 
-		// Modulo of 0 is NaN
-		if (a.length === 0) {
-			a[j] = x;
-		} else {
-			a[(j + n) % a.length] = x;
-		}
+    /** Modulo of 0 is NaN */
+    if (a.length === 0) {
+      a[j] = x;
+    } else {
+      a[(j + n) % a.length] = x;
+    }
 
-		n++;
-		return true;
-	};
-	
-	this.peek = function() {
-		var x = null;
-		if (n > 0) {
-			x = a[j];
-		}
-		return x;
-	};
+    n++;
+    return true;
+  };
 
-	this.remove = function() {
-		if (n === 0) return false;
+  this.peek = function() {
+    var x = null;
+    if (n > 0) {
+      x = a[j];
+    }
+    return x;
+  };
 
-		var x = a[j];
-		j = (j + 1) % a.length;
-		n--;
-		
-		if (n <= 3*n) resize();
+  this.remove = function() {
+    if (n === 0) return false;
 
-		return x; 
-	};
-	
-	this.poll = function() {
-		return n === 0 ? null : this.remove();
-	};
+    var x = a[j];
+    j = (j + 1) % a.length;
+    n--;
+
+    if (n <= 3*n) resize();
+
+    return x; 
+  };
+
+  this.poll = function() {
+    return n === 0 ? null : this.remove();
+  };
 }
 
 if (module && module.exports) module.exports = ArrayQueue;
